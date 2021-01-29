6f96db3949c777e4273914eadf4d16487082b2e2
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2021-01-28 20:57:02 -0500

    Vuex setup

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -10,11 +10,27 @@
       >
     </div>
     <h2>List</h2>
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
+    </ul>
+    <h3 v-else>No coaches</h3>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    filteredCoaches() {
+      // coaches: 1st is namespace in store, 2nd is the getters name
+      return this.$store.getters["coaches/coaches"];
+    },
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+// export default createStore({
+//   state: {
+//   },
+//   mutations: {
+//   },
+//   actions: {
+//   },
+//   modules: {
+//   }
+// })
+
+import coachesModule from "./modules/coaches/index.js";
+import requestsModule from "./modules/requests/index.js";
+
+const store = createStore({
   modules: {
-  }
-})
+    coaches: coachesModule,
+    requests: requestsModule,
+  },
+});
+
+export default store;
