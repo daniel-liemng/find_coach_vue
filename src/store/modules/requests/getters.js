f0425188dee132f1f4db5e723c200ec8ae2dcb14
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2021-02-02 22:34:27 -0500

    Filter request

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,8 +1,16 @@
 export default {
-  requests(state) {
-    return state.requests;
+  // requests(state) {
+  //   return state.requests;
+  // },
+  // hasRequests(state) {
+  //   return state.requests && state.requests.length > 0;
+  // },
+  requests(state, _, _2, rootGetters) {
+    const coachId = rootGetters.userId;
+
+    return state.requests.filter((req) => req.coachId === coachId);
   },
-  hasRequests(state) {
-    return state.requests && state.requests.length > 0;
+  hasRequests(_, getters) {
+    return getters.requests && getters.requests.length > 0;
   },
 };
