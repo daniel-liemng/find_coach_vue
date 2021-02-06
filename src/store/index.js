0d5bead950cf769feb2ccce710df0794078c65fb
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2021-02-06 13:25:04 -0500

    Auth Vuex setup

--- src/store/index.js
+++ src/store/index.js
@@ -13,23 +13,25 @@ import { createStore } from "vuex";
 
 import coachesModule from "./modules/coaches/index.js";
 import requestsModule from "./modules/requests/index.js";
+import authModule from "./modules/auth/index.js";
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
     requests: requestsModule,
+    auth: authModule,
   },
-  state() {
-    return {
-      // Authentication
-      userId: "c2",
-    };
-  },
-  getters: {
-    userId(state) {
-      return state.userId;
-    },
-  },
+  // state() {
+  //   return {
+  //     // Authentication
+  //     userId: "c2",
+  //   };
+  // },
+  // getters: {
+  //   userId(state) {
+  //     return state.userId;
+  //   },
+  // },
 });
 
 export default store;
--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -2,26 +2,26 @@ import mutations from "./mutations.js";
 import actions from "./actions.js";
 import getters from "./getters.js";
 
-const COACHES = [
-  {
-    id: "c1",
-    firstName: "Maximilian",
-    lastName: "Schwarzmüller",
-    areas: ["frontend", "backend", "career"],
-    description:
-      "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
-    hourlyRate: 30,
-  },
-  {
-    id: "c2",
-    firstName: "Julie",
-    lastName: "Jones",
-    areas: ["frontend", "career"],
-    description:
-      "I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.",
-    hourlyRate: 45,
-  },
-];
+// const COACHES = [
+//   {
+//     id: "c1",
+//     firstName: "Maximilian",
+//     lastName: "Schwarzmüller",
+//     areas: ["frontend", "backend", "career"],
+//     description:
+//       "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+//     hourlyRate: 30,
+//   },
+//   {
+//     id: "c2",
+//     firstName: "Julie",
+//     lastName: "Jones",
+//     areas: ["frontend", "career"],
+//     description:
+//       "I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.",
+//     hourlyRate: 45,
+//   },
+// ];
 
 export default {
   namespaced: true,
@@ -31,7 +31,7 @@ export default {
   //   };
   // },
   state: {
-    coaches: COACHES,
+    coaches: [],
     lastFetch: null,
   },
   mutations,
