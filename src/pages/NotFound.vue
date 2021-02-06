dce5bdbc57025ec62bab94931002f051b299f74c
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2021-02-06 11:21:36 -0500

    Not Found Page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,11 @@
 <template>
-  <h2>Not Found</h2>
+  <section class="container mt-5 text-center">
+    <h2>Not Found</h2>
+    <p class="mt-5">
+      Page Not found - Please check out all our
+      <router-link to="/coaches">coaches</router-link>
+    </p>
+  </section>
 </template>
 
 <script>
